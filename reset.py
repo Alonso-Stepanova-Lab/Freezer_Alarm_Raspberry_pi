665d9fca5fc3618072fea23fd97ed5a1360d4ba7
Alonso_Stepanova Lab <jmalonso@ncsu.edu>
2020-11-24 16:22:18 -0500

    Update reset.py

--- reset.py
+++ reset.py
@@ -3,6 +3,6 @@ import os
 try:
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    server.login("alonsobellverraspberrypi@gmail.com","TAronjes")
+    server.login("your email","your password")
 except:
     os.system("sudo reboot")
